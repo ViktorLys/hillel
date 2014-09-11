01eaee0cc4418a553d12dacfb1a70f68d994a9a0
Denis <denissaranuk@gmail.com>
2014-09-11 14:37:40 +0300

    ver 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Arthur :)!");
+        System.out.println("Hello, Arthur from Denis Saranyuk :)!");
     }
 
 
