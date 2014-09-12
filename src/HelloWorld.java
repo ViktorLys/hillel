2c7c2bbdc55b01baf6c61c5101d3825746542f8e
Stepurko Alexadr <Stepurko@mail.ru>
2014-09-12 10:05:37 +0300

    Update 20140912

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Arthur from Denis :)!");
-    }
+
+        System.out.println("Hello, Arthur from Denis : Alexandr!");
 }
