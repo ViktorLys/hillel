a3b3a9fe3bfb6916af262a8b0f03d5dd0226a380
Arthur <a.a.piluck@gmail.com>
2014-09-15 21:35:37 +0300

    fix

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,2 @@
-/**
- * Created with IntelliJ IDEA.
- * User: IT School
- * Date: 15.09.14
- * Time: 21:20
- * To change this template use File | Settings | File Templates.
- */
 public class Test {
 }
