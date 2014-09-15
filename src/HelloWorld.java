6f8ae227b4b5d2203579c5f6b829ad5d3c7a88e3
Nastya Hrankina <shumi@breezein.net>
2014-09-15 20:13:01 +0300

    fix

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,12 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
 
-        System.out.println("Hello, Arthur from Denis : Alexandr! :P");
-        System.out.println("Hello, Arthur from Kate :)!");
+    System.out.println("Hello, Arthur from Denis : Alexandr! :P");
+    System.out.println("Hello, Arthur from Kate :)!");
 
-        System.out.println("Hello, Arthur from Bogdan :)!");
-
-    }
+    System.out.println("Hello, Arthur from Bogdan :)!");
+    System.out.println("Hello, Arthur from Vitya !");
+  }
 }
