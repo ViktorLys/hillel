2255b909814830b2ccb25f7684ab25bcdd85f46b
business-pr-lady <salsalorien22>
2014-09-16 23:10:58 +0400

    kate.ver.1.1

--- src/Test_kkondrashova.java
+++ src/Test_kkondrashova.java
@@ -1,9 +1,37 @@
-
+import java.io.File;
+import java.io.IOException;
 
 public class Test_kkondrashova {
 
+  public static void createNewFile() throws IOException {
 
+        File f = null;
+        boolean bool = false;
 
+        try{
+            // create new file
+            f = new File("kkondrashova.txt");
+
+            // tries to create new file in the system
+            bool = f.createNewFile();
+
+            // prints
+            System.out.println("File kkondrashova.txt created: "+bool);
+
+            // deletes file from the system
+            f.delete();
+
+            // tries to create new file in the system
+            bool = f.createNewFile();
+
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+    }
 
 }
+
+
+
 
