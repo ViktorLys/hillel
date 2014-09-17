0b2ccb9032b7360d40692ab94be6588736a97867
business-pr-lady <salsalorien22>
2014-09-17 18:22:31 +0400

    ver.2

--- src/Test_kkondrashova.java
+++ src/Test_kkondrashova.java
@@ -3,35 +3,23 @@ import java.io.IOException;
 
 public class Test_kkondrashova {
 
-  public static void createNewFile() throws IOException {
 
-        File f = null;
-        boolean bool = false;
+    private String fileName = "kkondrashova.txt";
 
-        try{
-            // create new file
-            f = new File("kkondrashova.txt");
+    public static void createFile(String path, String filename) {
+        File file = new File(path, filename);
+        if (!file.exists()) {//
+            try {
+                file.createNewFile();  //
 
-            // tries to create new file in the system
-            bool = f.createNewFile();
-
-            // prints
-            System.out.println("File kkondrashova.txt created: "+bool);
-
-            // deletes file from the system
-            f.delete();
-
-            // tries to create new file in the system
-            bool = f.createNewFile();
-
-        }catch(Exception e){
-            e.printStackTrace();
-
+            } catch (IOException e) {
+                //
+                e.printStackTrace();
+            }
         }
+
     }
 
 }
-
-
 
 
